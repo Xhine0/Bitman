e6e83e033eb524fcf34062d3e8c47e7655be8a88
nuc006 <nuc006@student.uib.no>
2017-09-06 16:23:02 +0200

    Add project

---
+++ ProgrammingContest II (06.09.17)/src/WorkStations.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class WorkStations {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		
+		int n = reader.nextInt();
+		int m = reader.nextInt();
+		
+		List<Integer> list1 = new ArrayList<>();
+		for(int i = 0; i < n; i++) {
+			
+		}
+	}
+
+}
